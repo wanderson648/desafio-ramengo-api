d8ea404176f8383a17386d6c93cbf5cce7b7b61a
wanderson648 <testepedroca@gmail.com>
2024-06-12 15:59:54 -0300

    init  application

---
+++ src/main/java/br/com/wo/ramengo_api/desafio_backend_rv/DesafioBackendRvApplication.java
@@ -0,0 +1,13 @@
+package br.com.wo.ramengo_api.desafio_backend_rv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioBackendRvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioBackendRvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/wo/ramengo_api/desafio_backend_rv/DesafioBackendRvApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wo.ramengo_api.desafio_backend_rv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioBackendRvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
